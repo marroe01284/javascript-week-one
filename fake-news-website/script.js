abe9ac4d0864867e20cebe36ec2cf1397f3c31b2
Marius Rønning <143086706+marroe01284@users.noreply.github.com>
2024-01-11 12:12:57 +0100

    ok

---
+++ fake-news-website/script.js
@@ -0,0 +1,83 @@
+let news = [
+{
+    headline: "Watch Hesh landing on the moon",
+    summary: "he landed with his super jet" 
+},
+{
+    headline: "This news is crazy as hell",
+    summary: "the world as we know it is going to explode" 
+},
+{
+    headline: "summer games 2024",
+    summary: "the time is here we are going to compete as warriors" 
+},
+];
+
+console.log(news);
+
+//  let newsContainer = document.querySelector(".news");
+// // //  console.log("newsContainer:", newsContainer);
+
+// // //  const totalNumbersNews = news.length;
+// // //  console.log(totalNumbersNews);
+// // //  function html (newsArrey){}
+// // // let html = "";
+// // //  for (let i = 0; i < totalNumbersNews; i++){
+// // //  console.log(news[i].headline);
+
+// // //  html = html + `
+// // // <div>
+// // //  <h2>${news[i].headline} </h2>
+// // //  <p> ${news[i].summary} </p>
+// // //  </div>
+// // // `
+// // //  console.log(html);
+// // // }
+// // //  newsContainer.innerHTML = html;
+const cats = [
+    {
+
+        name: "moshu",
+        age: 2,
+    },
+    {
+
+        name: "titty",
+        age: 5,
+    },
+    {
+
+        name: "dodo",
+        age: 4,
+    },
+];
+function getName(list){
+for( let i = 0; i < list.length; i++){
+    console.log(list[i].name);
+}
+}
+// for (let i = 0; i < cats.length; i++){
+// console.log(cats[i].name);
+// }
+
+getName(cats);
+
+const dogs = [
+    {
+
+        name: "moshu",
+        age: 2,
+    },
+    {
+
+        name: "titty",
+        age: 5,
+    },
+    {
+
+        name: "dodo",
+        age: 4,
+    },
+];
+getName(dogs);
+
